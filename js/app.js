2023f99d44655c8379354e844d06980d21ffcd98
Keishi Asai <isheik808@gmail.com>
2017-10-14 17:40:39 -0700

    made base files

---
+++ js/app.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "react-dom";
+
+const App = () => (
+    <div>
+        <h1>test</h1>
+    </div>
+);
+
+export default App;
---
+++ js/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "react-dom";
+import { App } from './app';
+
+ReactDOM.render(
+    document.getElementById("app")
+);
+
+
