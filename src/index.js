237913fbbf1bb03f2c06ca309f33f568bcaa92a8
Keishi Asai <isheik808@gmail.com>
2017-10-14 21:25:29 -0700

    prepare dev env

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
-import { App } from './app';
+import App from './app';
 
-ReactDOM.render(
+render(<App />,
     document.getElementById("app")
 );
-
-
---
+++ src/testout.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { render } from "react-dom";
+
+const App = () => React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "test"
+    )
+);
+
+export default App;
+
