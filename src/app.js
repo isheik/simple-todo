4c2c6cda928b94cbf4d53b7bcf3bd45200e476d9
Keishi Asai <isheik808@gmail.com>
2017-10-14 23:15:53 -0700

    practice

--- src/app.js
+++ src/app.js
@@ -1,9 +1,65 @@
 import React from "react";
 import { render } from "react-dom";
 
+var createReactClass = require('create-react-class');
+
+var todos = [{
+    id: '_1',
+    name: 'Buy some milk',
+    done: true
+}, {
+    id: '_2',
+    name: 'Birthday present to Alice',
+    done: false
+}];
+
+var Todo = createReactClass({
+    render: function() {
+        var todo = this.props.todo;
+        return (<li>{todo.name}<button>Done</button></li>);
+    }
+});
+
+var TodoList = createReactClass({
+    render: function(){
+        var rows = this.props.todos.filter(function(todo){
+            return !todo.done;
+        }).map(function(todo){
+            return(<Todo key={todo.id} todo={todo}></Todo>);
+        });
+        return (
+            <div className="active-todos">
+                <h2>Active</h2>
+                <ul>{rows}</ul>
+            </div>
+        );
+    }
+});
+
+var TodoForm = createReactClass({
+    handleSubmit: function(e){
+        e.preventDefault();
+        var name = this.refs.todoName.getDOMNode().value.trim();
+        if(name){
+            aleat(name);
+            this.refs.todoName.getDOMNode().value = '';
+        }
+    },
+    render: function(){
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input ref="todoName"></input><input type="submit"></input>
+
+            </form>
+        );
+    }
+});
+
+
 const App = () => (
     <div>
-        <h1>test</h1>
+        <h1>My Todo</h1>
+        <TodoList todos={todos}/>
     </div>
 );
 
